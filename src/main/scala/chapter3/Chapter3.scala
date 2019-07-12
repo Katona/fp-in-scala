1a49e217dbec098662ab5785881dcc8208140cde
horngab <gabor.hornyak@msci.com>
2019-07-12 17:42:39 +0200

    Cleanup

---
+++ src/main/scala/chapter3/Chapter3.scala
@@ -0,0 +1,38 @@
+package chapter3
+
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+}
+
+object Excercise2 {
+
+  def tail[A](list: List[A]): List[A] = list match {
+    case Nil => Nil
+    case Cons(h, t) => t
+  }
+}
+
+object Excercise3 {
+
+  def drop[A](list: List[A], n: Int): List[A] = list match {
+    case Nil => Nil
+    case Cons(h, t) if n > 0 => drop(t, n - 1)
+    case l: List[A] => l
+  }
+}
+
+object Excercise4 {
+  def dropWhile[A](list: List[A])(predicate: A => Boolean): List[A] = list match {
+    case Nil => Nil
+    case Cons(h, t) if predicate(h) => dropWhile(t)(predicate)
+    case l: List[A] => l
+  }
+}
--- src/main/scala/chapter3/Excercise2.scala
+++
@@ -1,9 +0,0 @@
-package chapter3
-
-object Excercise2 {
-
-  def tail[A](list: List[A]): List[A] = list match {
-    case Nil => Nil
-    case Cons(h, t) => t
-  }
-}
