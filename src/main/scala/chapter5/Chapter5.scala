6ecfc35d25ec57b8c972a5bbd0a274bdf84a30b4
horngab <gabor.hornyak@msci.com>
2019-10-08 17:30:18 +0200

    toList

--- src/main/scala/chapter5/Chapter5.scala
+++ src/main/scala/chapter5/Chapter5.scala
@@ -1,9 +1,30 @@
 package chapter5
 
+import scala.collection.mutable.ListBuffer
+
 trait Stream[+A] {
     def headOption: Option[A] = this match {
         case Empty => None
         case Cons(h, t) => Some(h())
+    }
+    
+    def toList: List[A] = {
+        def go(s: Stream[A], acc: List[A]): List[A] = s match {
+            case Empty => acc
+            case Cons(h, t) => go(t(), h() :: acc)
+        }
+        go(this, Nil).reverse
+    }
+
+    def toListFast: List[A] = {
+        val buf = new ListBuffer[A]
+        def go(s: Stream[A]): List[A] = s match {
+            case Cons(h, t) =>
+                buf += h()
+                go(t())
+            case Empty => buf.toList
+        }
+        go(this)
     }
 }
 
