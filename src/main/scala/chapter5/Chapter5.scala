aa62eebbb868d308bc2fae3fbe3ee7078764d13d
horngab <gabor.hornyak@msci.com>
2019-10-09 11:23:26 +0200

    5.2 take & drop

--- src/main/scala/chapter5/Chapter5.scala
+++ src/main/scala/chapter5/Chapter5.scala
@@ -26,6 +26,18 @@ trait Stream[+A] {
         }
         go(this)
     }
+
+    final def take(n: Int): Stream[A] = this match {
+        case Cons(h, t) if n > 1 => Stream.cons(h(), t().take(n - 1))
+        case Cons(h, _) if n == 1 => Stream.cons(h(), Stream.empty)
+        case _ => Empty
+    }
+
+    @annotation.tailrec
+    final def drop(n: Int): Stream[A] = this match {
+        case Cons(_, t) if n > 0 => t().drop(n - 1)
+        case _ => this
+    }
 }
 
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
